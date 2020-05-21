acff52be4c86f6e61404f27a470857caf31d5b47
Felipe Cardoso <felipedclc@gmail.com>
2020-05-21 16:27:01 -0300

    Combo device

--- src/application/program.java
+++ src/application/program.java
@@ -1,22 +1,26 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretPrinter;
+import devices.ConcretScanner;
 
 public class program {
 
 	public static void main(String[] args) {
 		
-		Printer p1 = new Printer("1008");
+		ConcretPrinter p1 = new ConcretPrinter("1008");
 		p1.processDoc("My letter");
 		p1.print("My letter");
 		
 		System.out.println();
-		Scanner s1 = new Scanner("1181");
+		ConcretScanner s1 = new ConcretScanner("1181");
 		s1.processDoc("My email");
 		System.out.println("Scan result: " + s1.scan());
 		
-		
+		ComboDevice c1 = new ComboDevice("2314");
+		c1.processDoc("My dissertation");
+		c1.print("My dissertation");
+		System.out.println("Scan result: " + c1.scan());
 		
 		
 	}
---
+++ src/devices/ConcretScanner.java
@@ -0,0 +1,17 @@
+package devices;
+
+public class ConcretScanner extends Device implements Scanner {
+
+	public ConcretScanner(String serialNumber) {
+		super(serialNumber);
+	}
+
+	@Override
+	public void processDoc(String doc) {
+		System.out.println("Scanner processing: " + doc);
+	}
+
+	public String scan() {
+		return "Sanned content";
+	}
+}
